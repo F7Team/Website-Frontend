f3fb72f478d667cce21b6d6ed5dafe1e2ec64bf5
James Ade <60360836+JamesHardey@users.noreply.github.com>
2023-10-25 05:05:07 -0700

    App Navbar implementation

--- src/components/appnavbar/js/AppNavBar.js
+++ src/components/appnavbar/js/AppNavBar.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../logo-image.svg';
-import '../css/AppNavBar.css'
+import '../css/AppNavBar.css';
 
 const AppNavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="container">
         <div className="logo">
           <img className='logo-image' src={logo} alt="Logo" />
         </div>
-        <ul className="nav-links">
+        <div className="menu-button" onClick={toggleMenu}>
+          <div className={`menu-icon ${menuOpen ? 'open' : ''}`}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+        <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
           <li><a href="#home">Home</a></li>
           <li><a href="#services">Services</a></li>
           <li><a href="#about">About Us</a></li>
@@ -21,4 +34,3 @@ const AppNavBar = () => {
 };
 
 export default AppNavBar;
-
