aab963d7730b0e494c3006fabec4167afff34724
James Ade <60360836+JamesHardey@users.noreply.github.com>
2023-11-15 12:54:04 -0800

    update code

--- src/pages/home/hero/Hero.js
+++ src/pages/home/hero/Hero.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import heroImage from './hero-image.png'
 import './Hero.css'; // Import your CSS file for styling
 
